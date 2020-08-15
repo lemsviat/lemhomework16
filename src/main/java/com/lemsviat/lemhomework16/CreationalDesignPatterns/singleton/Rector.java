5eac549afbc00e04676b8063cbc95dc54423e257
Sviatoslav Lementar <lemsviat@gmail.com>
2020-08-15 13:48:03 +0300

    Singleton

---
+++ src/main/java/com/lemsviat/lemhomework16/CreationalDesignPatterns/singleton/Rector.java
@@ -0,0 +1,23 @@
+package main.java.com.lemsviat.lemhomework16.CreationalDesignPatterns.singleton;
+
+public final class Rector { //Rector is head of some university
+    private static volatile Rector instance;
+    public String name;
+
+    private Rector(String name) {
+        this.name = name;
+    }
+    public static Rector getInstance(String name) {
+
+        Rector result = instance;
+        if (result != null) {
+            return result;
+        }
+        synchronized(Rector.class) {
+            if (instance == null) {
+                instance = new Rector(name);
+            }
+            return instance;
+        }
+    }
+}
---
+++ src/main/java/com/lemsviat/lemhomework16/CreationalDesignPatterns/singleton/SingletonDemo.java
@@ -0,0 +1,29 @@
+package main.java.com.lemsviat.lemhomework16.CreationalDesignPatterns.singleton;
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (correct)." + "\n" +
+                "If you see different values, then 2 singletons were created (fail)." + "\n\n" +
+                "RESULT:" + "\n");
+        Thread threadOne = new Thread(new ThreadCandidateFirst());
+        Thread threadTwo = new Thread(new ThreadCandidateSecond());
+        threadOne.start();
+        threadTwo.start();
+    }
+
+    static class ThreadCandidateFirst implements Runnable {
+        @Override
+        public void run() {
+            Rector candidate1 = Rector.getInstance("Ivan Ivanovich");
+            System.out.println(candidate1.name);
+        }
+    }
+
+    static class ThreadCandidateSecond implements Runnable {
+        @Override
+        public void run() {
+            Rector candidate2 = Rector.getInstance("Petr Petrovich");
+            System.out.println(candidate2.name);
+        }
+    }
+}
